4d1a3b314e131fecdf2683071028fbcc46035bed
Kalisone <emorelos04@gmail.com>
2025-10-09 04:15:11 +0000

    added autoclicker (STEP 3 DONE)
    uses variables to allow interval delay & increment amount to be changed

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,21 @@ document.body.innerHTML = `
 `;
 
 let counter: number = 0;
+// deno-lint-ignore prefer-const
+let autoclickDelay: number = 1000;
+// deno-lint-ignore prefer-const
+let autoclickIncrement: number = 1;
 
 const button = document.getElementById("increment")!;
 const counterElement = document.getElementById("counter")!;
 
+// Click Listener for Button
 button.addEventListener("click", () => {
   counterElement.textContent = String(++counter);
 });
+
+// "Autoclicker" Counter increments by (1) star(s) every (1) second(s)
+setInterval(() => {
+  counter += autoclickIncrement;
+  counterElement.innerHTML = String(counter);
+}, autoclickDelay);
