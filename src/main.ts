43397e2dfc2918047aa8cfdad8c43a75ec46697a
Kalisone <emorelos04@gmail.com>
2025-10-09 03:54:39 +0000

    added counter label for button (STEP 2 DONE)

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ import "./style.css";
 document.body.innerHTML = `
   <h1>CMPM 121 Demo 1</h1>
   <button id="increment">⭐</button>
+  <div>You have <span id="counter">0</span> stars!</div>
 `;
+
+let counter: number = 0;
+
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  counterElement.textContent = String(++counter);
+});
