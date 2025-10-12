19bc09f3d46868fc873d7e7b13592d6080c675c3
Kalisone <emorelos04@gmail.com>
2025-10-12 08:10:27 +0000

    added second upgrade

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ document.body.innerHTML = `
   <h1>CMPM 121 Demo 1</h1>
   <button id="incrementID">⭐</button>
   <div>You have <span id="counterStarsID">0</span> stars!</div>
-  <button id="upgradeOneID">Buy telescope: <span id="costTelescopesID">X</span> stars</button>
+  <button id="buttonTelescopesID">Buy telescope: <span id="costTelescopesID">X</span> stars</button>
   <div>Number of telescopes: <span id="counterTelescopesID">0</span></div>
 `;
 
 let counterStars: number = 0;
+
 let counterTelescopes: number = 0;
 let costTelescopes: number = 10;
+
+let counterResearchCenters: number = 0;
+let costResearchCenters: number = 100;
 
 let clickIncrement: number = 1;
 // deno-lint-ignore prefer-const
@@ -22,9 +26,18 @@ let autoclickIncrement: number = 1;
 
 const buttonStars = document.getElementById("incrementID")!;
 const counterElemStars = document.getElementById("counterStarsID")!;
-const buttonTelescopes = document.getElementById("upgradeOneID")!;
+const buttonTelescopes = document.getElementById("buttonTelescopesID")!;
 const costElemTelescopes = document.getElementById("costTelescopesID")!;
 const counterElemTelescopes = document.getElementById("counterTelescopesID")!;
+const buttonResearchCenters = document.getElementById(
+  "buttonResearchCenterID",
+)!;
+const costElemResearchCenters = document.getElementById(
+  "costResearchCenterID",
+)!;
+const counterElemResearchCenters = document.getElementById(
+  "counterResearchCenterID",
+)!;
 
 costElemTelescopes.textContent = String(costTelescopes);
 
@@ -37,7 +50,7 @@ buttonStars.addEventListener("click", () => {
 // Click Listener for Telescope Upgrade Button
 buttonTelescopes.addEventListener("click", () => {
   if (counterStars >= costTelescopes) {
-    clickIncrement++;
+    clickIncrement = Math.floor(clickIncrement * 1.1);
 
     counterStars -= costTelescopes;
     counterElemStars.textContent = String(counterStars);
@@ -45,6 +58,20 @@ buttonTelescopes.addEventListener("click", () => {
     costTelescopes = Math.floor(costTelescopes * 1.5);
     costElemTelescopes.textContent = String(costTelescopes);
     counterElemTelescopes.textContent = String(++counterTelescopes);
+  }
+});
+
+// Click Listener for Research Center Upgrade Button
+buttonResearchCenters.addEventListener("click", () => {
+  if (counterStars >= costResearchCenters) {
+    clickIncrement = Math.floor(clickIncrement * 2.0);
+
+    counterStars -= costResearchCenters;
+    counterElemStars.textContent = String(counterStars);
+
+    costResearchCenters = Math.floor(costResearchCenters * 1.5);
+    costElemResearchCenters.textContent = String(costResearchCenters);
+    counterElemResearchCenters.textContent = String(++counterResearchCenters);
   }
 });
 
