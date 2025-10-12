05c89f33bb2332ed65c5595cbc7af86d03f5ce50
Kalisone <emorelos04@gmail.com>
2025-10-12 08:50:17 +0000

    modified upgrade 2 to increase autoclick increment

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ document.body.innerHTML = `
   <h1>CMPM 121 Demo 1</h1>
   <button id="incrementID">⭐</button>
   <div>You have <span id="counterStarsID">0</span> stars!</div>
-  <button id="buttonTelescopesID">Buy telescope: <span id="costTelescopesID">X</span> stars</button>
+  <button id="buttonTelescopesID">Buy Telescope: <span id="costTelescopesID">X</span> stars</button>
   <div>Number of telescopes: <span id="counterTelescopesID">0</span></div>
+  <button id="buttonResearchCentersID">Buy Research Center: <span id="costResearchCentersID">X</span> stars</button>
+  <div>Number of research centers: <span id="counterResearchCentersID">0</span></div>
 `;
 
 let counterStars: number = 0;
@@ -14,64 +16,70 @@ let counterTelescopes: number = 0;
 let costTelescopes: number = 10;
 
 let counterResearchCenters: number = 0;
-let costResearchCenters: number = 100;
+let costResearchCenters: number = 10;
 
 let clickIncrement: number = 1;
 // deno-lint-ignore prefer-const
 let autoclickDelay: number = 1000;
-// deno-lint-ignore prefer-const
 let autoclickIncrement: number = 1;
 
 // BUTTONS
 
 const buttonStars = document.getElementById("incrementID")!;
 const counterElemStars = document.getElementById("counterStarsID")!;
+
 const buttonTelescopes = document.getElementById("buttonTelescopesID")!;
 const costElemTelescopes = document.getElementById("costTelescopesID")!;
 const counterElemTelescopes = document.getElementById("counterTelescopesID")!;
+
 const buttonResearchCenters = document.getElementById(
-  "buttonResearchCenterID",
+  "buttonResearchCentersID",
 )!;
 const costElemResearchCenters = document.getElementById(
-  "costResearchCenterID",
+  "costResearchCentersID",
 )!;
 const counterElemResearchCenters = document.getElementById(
-  "counterResearchCenterID",
+  "counterResearchCentersID",
 )!;
 
 costElemTelescopes.textContent = String(costTelescopes);
+costElemResearchCenters.textContent = String(costResearchCenters);
 
 // Click Listener for Star Button
 buttonStars.addEventListener("click", () => {
   counterStars += clickIncrement;
-  counterElemStars.textContent = String(counterStars);
+  counterElemStars.textContent = String(Math.round(counterStars));
 });
 
 // Click Listener for Telescope Upgrade Button
 buttonTelescopes.addEventListener("click", () => {
   if (counterStars >= costTelescopes) {
-    clickIncrement = Math.floor(clickIncrement * 1.1);
+    clickIncrement++;
 
     counterStars -= costTelescopes;
-    counterElemStars.textContent = String(counterStars);
+    counterElemStars.textContent = String(Math.round(counterStars));
 
-    costTelescopes = Math.floor(costTelescopes * 1.5);
-    costElemTelescopes.textContent = String(costTelescopes);
-    counterElemTelescopes.textContent = String(++counterTelescopes);
+    costTelescopes = costTelescopes * 1.5;
+    costElemTelescopes.textContent = String(Math.round(costTelescopes));
+    counterElemTelescopes.textContent = String(Math.round(++counterTelescopes));
   }
 });
 
 // Click Listener for Research Center Upgrade Button
 buttonResearchCenters.addEventListener("click", () => {
   if (counterStars >= costResearchCenters) {
-    clickIncrement = Math.floor(clickIncrement * 2.0);
+    autoclickIncrement += 0.1;
 
     counterStars -= costResearchCenters;
-    counterElemStars.textContent = String(counterStars);
+    counterElemStars.textContent = String(Math.round(counterStars));
 
-    costResearchCenters = Math.floor(costResearchCenters * 1.5);
-    costElemResearchCenters.textContent = String(costResearchCenters);
-    counterElemResearchCenters.textContent = String(++counterResearchCenters);
+    costResearchCenters = costResearchCenters * 1.1;
+    costElemResearchCenters.textContent = String(
+      Math.round(costResearchCenters),
+    );
+    counterElemResearchCenters.textContent = String(
+      Math.round(++counterResearchCenters),
+    );
   }
 });
 
@@ -88,7 +96,7 @@ function autoclick(timestamp: number) {
 
   while (timeAccumulator >= autoclickDelay) {
     counterStars += autoclickIncrement;
-    counterElemStars.textContent = String(counterStars);
+    counterElemStars.textContent = String(Math.round(counterStars));
 
     timeAccumulator -= autoclickDelay;
   }
